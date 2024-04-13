cf98c4f009d715af7fe6082d5df81302a699ca91
Gerardo <gerardocamarillodl@gmail.com>
2024-04-12 22:10:02 -0600

    Comunicando componentes con props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react'
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
@@ -6,23 +7,33 @@ import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 
 
+const defaultTodos = [
+  { text: "Sample text 1", completed: true },
+  { text: "Sample text 2", completed: false },
+  { text: "Sample text 3", completed: true },
+]
+
 function App() {
   return (
-    <div className="App">
-      <body>
-        <TodoCounter />
-        <TodoSearch />
+    <React.Fragment>
+      <TodoCounter completed={2} total={3} />
+      <TodoSearch />
 
-        <TodoList>
-          <TodoItem />
-          <TodoItem />
-          <TodoItem />
-        </TodoList>
+      <TodoList >
+        {defaultTodos.map(
+          todo => <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed} />
+        )}
 
-        <CreateTodoButton />
 
-      </body>
-    </div>
+
+      </TodoList>
+
+      <CreateTodoButton />
+
+    </React.Fragment >
   );
 }
 
